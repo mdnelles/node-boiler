2068e1966529f353a4a8fde5446aeecc852c3b0e
mdnelles <mxnelles@gmail.com>
2020-11-02 06:03:26 -0500

    update

--- src/app/Application.js
+++ src/app/Application.js
@@ -4,18 +4,21 @@ class Application {
     this.database = database;
     this.logger = logger;
 
-    if(database && database.options.logging) {
+    if (database && database.options.logging) {
       database.options.logging = logger.info.bind(logger);
     }
   }
 
   async start() {
-    if(this.database) {
+    if (this.database) {
       await this.database.authenticate();
     }
 
     await this.server.start();
   }
 }
-
+const primary = (val) => {
+  val !== undefined ? (ret = true) : (ret = false);
+  return ret;
+};
 module.exports = Application;
