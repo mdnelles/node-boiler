e5ef53667ecf99913a16d30edf1e124f282b4a2d
mdnelles <mxnelles@gmail.com>
2021-01-27 09:12:43 -0500

    update

---
+++ src/reducers/todo.js
@@ -0,0 +1,11 @@
+const todoAppState = {
+  todos: [
+    { id: 0, text: "Learn React", completed: true },
+    { id: 1, text: "Learn Redux", completed: false, color: "purple" },
+    { id: 2, text: "Build something fun!", completed: false, color: "blue" },
+  ],
+  filters: {
+    status: "Active",
+    colors: ["red", "blue"],
+  },
+};
