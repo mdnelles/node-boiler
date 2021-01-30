231a649986d93eccf876c2fa230ec43581f3985f
mdnelles <mxnelles@gmail.com>
2021-01-29 20:31:47 -0500

    update

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -2,7 +2,6 @@ const todoAppState = {
   todos: [
     { id: 0, text: "install", completed: true },
     { id: 1, text: "debug", completed: false, color: "purple" },
-    { id: 2, text: "reset", completed: false, color: "blue" },
   ],
   filters: {
     status: "Active",
